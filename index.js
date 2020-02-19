4ea554cd21f8ad6596bc579c127b16bb2ed84f96
cayenne-pepper <ju_silva@icloud.com>
2020-02-19 17:17:49 +0100

    Add layout and finish Pug Template

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(shopRouter);
 
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "PAge Not Found" });
 });
 
 const port = process.env.PORT || 4000;
