17dd65ca0b119be8ad992515051adf9371021601
cayenne-pepper <ju_silva@icloud.com>
2020-02-21 14:52:20 +0100

    Register routes

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 
 const router = new Router();
 
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
-router.get("/add-product", productsController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
 
-router.post("/add-product", productsController.postAddProduct);
+router.get("/products", adminController.getProducts);
+
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;
